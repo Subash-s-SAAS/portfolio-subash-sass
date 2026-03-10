ccc0962031e45ef4bde542d78606acb741f158cd
Subash <subashsuni55@gmail.com>
2026-03-10 10:27:18 +0530

    fix: footer property

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,13 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <Row as="footer" fillWidth padding="8" horizontal="center" s={{ direction: "column" }}>
+    <Row
+      as="footer"
+      fillWidth
+      padding="8"
+      horizontal="center"
+      s={{ direction: "column" }}
+    >
       <Row
         className={styles.mobile}
         maxWidth="m"
@@ -15,10 +21,10 @@ export const Footer = () => {
         gap="16"
         horizontal="between"
         vertical="center"
+        center={true}
         s={{
           direction: "column",
           horizontal: "center",
-          align: "center",
         }}
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
@@ -27,7 +33,9 @@ export const Footer = () => {
           <Text onBackground="neutral-weak">
             {/* Usage of this template requires attribution. Please don't remove the link to Once UI unless you have a Pro license. */}
             / Build your portfolio with{" "}
-            <SmartLink href="https://once-ui.com/products/magic-portfolio">Once UI</SmartLink>
+            <SmartLink href="https://once-ui.com/products/magic-portfolio">
+              Once UI
+            </SmartLink>
           </Text>
         </Text>
         <Row gap="16">
